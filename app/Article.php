0c384867008f40a991598acda7f393ea6fd10522
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-01-05 18:34:24 +0900

    リレーションを追加

--- app/Article.php
+++ app/Article.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
@@ -16,5 +17,19 @@ class Article extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
+    }
+
+    // ArticleモデルからUserモデルにアクセス可能にする
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    // ユーザーが「いいね」済みかどうかを判定する
+    public function isLikedBy(?User $user): bool
+    {
+        return $user
+            ? (bool)$this->likes->where('id', $user->id)->count()
+            : false;
     }
 }
