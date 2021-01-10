ba736079ea288d6ccfd7376cdc1653a8dc32fef5
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-01-10 17:38:31 +0900

    タグ別記事一覧画面のアクションメソッドを作成

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    //
+    // タグ別記事一覧画面のアクションメソッドを作成
+    public function show(string $name)
+    {
+        $tag = Tag::where('name', $name)->first();
+
+        return view('tags.show', ['tag' => $tag]);
+    }
 }
