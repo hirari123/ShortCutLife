40deec0f9a43ebb73746de078a95f0c86173c8fd
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-01-12 22:29:28 +0900

    新たにforo-tyuuno

---
+++ resources/views/users/followings.blade.php
@@ -0,0 +1,19 @@
+<!-- フォロー中一覧画面 -->
+
+@extends('app')
+
+@section('title', $user->name . 'のフォロー中')
+
+@section('content')
+  @include('nav')
+  <div class="container">
+    @include('users.user')
+    
+    @include('users.tabs', ['hasArticles' => false, 'hasLikes' => false])
+
+    @foreach($followings as $person)
+      @include('users.person')
+    @endforeach
+  </div>
+
+@endsection
