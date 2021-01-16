7695efb1c49feacd6ab76f22b19488efc1dd2f33
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-01-16 21:18:40 +0900

    ゲストログインの処理を記述

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -37,4 +38,31 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    // // ログイン後のリダイレクト先を指定
+    // public function redirectPath()
+    // {
+    //     return '/articles/index';
+    // }
+
+    // ゲストユーザー用のユーザーIDを定数として定義
+    private const GUEST_USER_ID = 1;
+
+    // ゲストログイン処理
+    public function guestLogin()
+    {
+        // id = 1のゲストユーザー情報がDBに存在すれば、ゲストログインする
+        if (Auth::loginUsingId(self::GUEST_USER_ID)) {
+            return redirect('/');
+        }
+
+        return redirect('/');
+    }
+
+    // ゲストログインを追加
+    // public function guestLogin(Request $request)
+    // {
+    //     Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('password')]);
+    //     return recirect('/');
+    // }
 }
