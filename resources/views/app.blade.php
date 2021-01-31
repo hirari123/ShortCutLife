9e139b8e2b9d442dfd6cd1ae5d0c534f81b0158c
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-01-31 15:03:49 +0900

    faviconを挿入

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,9 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 
+    <!-- favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/icon1.ico') }}" type="image/x-icon">
+
     <!-- Bootstrap core CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 
