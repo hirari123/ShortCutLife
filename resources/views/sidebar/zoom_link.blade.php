eb359de485d118050e4e59863f149868614a572b
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-02-06 20:55:31 +0900

    zoomとメインタグのファイルを新たに追加

---
+++ resources/views/sidebar/zoom_link.blade.php
@@ -0,0 +1,10 @@
+<!-- Zoomのリンク -->
+
+<div class="card mb-4">
+  <div class="btn blue-gradient text-center m-0">
+    <a href="" class="text-white">
+      <p class="h4">Zoom練習会</p>
+      <p class="h1 mb-0"><i class="fas fa-video"></i></p>
+    </a>
+  </div>
+</div>
