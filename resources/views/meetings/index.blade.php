df258dd16db02584e346c4d929a67fcaaa7a9f6d
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-02-12 19:03:17 +0900

    新たにミーティング一覧画面を作成

---
+++ resources/views/meetings/index.blade.php
@@ -0,0 +1,18 @@
+<!-- ミーティング一覧 -->
+
+@extends('app')
+
+@section('title', 'ミーティング一覧 | CHOT SHOT')
+
+@section('content')
+
+  @include('nav')
+
+  <div class="container mt-4">
+    <div class="row">
+      <div class="mx-auto col-md-7">
+        
+      </div>
+    </div>
+  </div>
+@endsection
