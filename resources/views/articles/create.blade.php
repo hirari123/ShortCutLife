329a243ac0185491dcf413ded0be8ccf9239fe95
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-02-15 22:58:41 +0900

    レイアウトの変更

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -7,16 +7,31 @@
 @include('nav')
 
 @section('content')
-  <div class="container">
+  <div class="container my-5">
     <div class="row">
-      <div class="col-12">
-        <div class="card mt-3">
-          <div class="card-body pt-0">
+      <div class="mx-auto col-md-7">
+        <div class="card">
+          <h2 class="h4 card-header text-center purple-gradient text-white">練習日を決めよう!!</h2>
+          <div class="card-body pt-3">
             @include('error_card_list')
             <div class="card-text">
               <form method="POST" action="{{ route('articles.store') }}">
                 @include('articles.form')
-                <button type="submit" class="btn blue-gradient btn-block">投稿する</button>
+                <div class="w-75 mx-auto d-flex justify-content-between align-items-start">
+                  <!-- 送信ボタン -->
+                  <div style="width: 45%;">
+                    <button type="submit" class="btn purple-gradient btn-block">
+                      <span class="h3">送 信</span>
+                    </button>
+                  </div>
+
+                  <!-- 戻るボタン -->
+                  <div style="width: 45%;">
+                    <button type="button" class="btn btn-outline-primary btn-block">
+                      <span class="h6">戻 る</span>
+                    </button>
+                  </div>
+                </div>
               </form>
             </div>
           </div>
