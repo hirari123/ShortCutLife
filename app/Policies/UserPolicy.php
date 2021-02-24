bcb40edb9ab96713fff90cd487f8238a2ad3b7dc
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-02-24 19:50:01 +0900

    新たにUserPolicyを作成

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function update(User $user, User $model)
+    {
+        return $user->id == $model->id;
+    }
+}
