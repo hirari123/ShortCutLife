ee885e5c160cdcd737d8cfde018c1e27b19f1126
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-02-25 14:45:51 +0900

    軽微な修正

--- resources/views/meetings/sppiner.blade.php
+++ resources/views/meetings/sppiner.blade.php
@@ -1,9 +1,9 @@
-<!-- 記事の無限スクロール -->
+<!-- ミーティングの無限スクロール -->
 
 @if ($meetings->nextPageUrl())
   <a href="{{ $meetings->nextPageUrl() }}" infinity-scroll>
     <div class="d-flex justify-content-center my-4">
-      <div class="spinner-border" role="status">
+      <div class="sppiner-border" role="status">
         <span class="sr-onry">Loading...</span>
       </div>
     </div>
