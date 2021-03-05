baf5038562fd3998165ff0e65319ad1f9c245080
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-03-05 21:05:04 +0900

    削除ファイルとその他

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -58,14 +58,10 @@ class LoginController extends Controller
     }
 
 
-    // ゲストユーザー用のユーザーIDを定数として定義
-    private const GUEST_USER_ID = 1;
-
     // ゲストログイン処理
     public function guestLogin()
     {
-        // id = 1のゲストユーザー情報がDBに存在すれば、ゲストログインする
-        if (Auth::loginUsingId(self::GUEST_USER_ID)) {
+        if (Auth::loginUsingId(config('user.guest_user_id'))) {
             session()->flash('msg_success', 'ゲストユーザーでログインしました!!');
             return redirect('/');
         }
