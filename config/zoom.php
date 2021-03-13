3754298a1529a795dc33651c2be3f8659b3a4fef
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-03-13 09:40:43 +0900

    configで管理する

--- config/zoom.php
+++ config/zoom.php
@@ -5,4 +5,15 @@ return [
   'zoom_api_key' => env('ZOOM_API_KEY'),
   'zoom_api_secret' => env('ZOOM_API_SECRET'),
   'zoom_account_email' => env('ZOOM_ACCOUNT_EMAIL'),
+  'meeting_status' => [
+    'active' => 'started',
+    'inactive' => 'waiting'
+  ],
+  
+  'meeting_type' => [
+    'instant_meeting' => 1,
+    'scheduled_meeting' => 2,
+    'recurring_meeting_with_no_fixed_time' => 3,
+    'recurring_meeting_with_fixed_time' => 8
+  ]
 ];
