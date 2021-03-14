e792ee0e103c8eb4996d177fd672286227668f37
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-03-14 22:58:15 +0900

    スクリプトタグをbladeに分離

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -52,22 +52,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
     <!-- flashMessage -->
-    <script>
-      // 成功時
-      @if (session('msg_success'))
-        $(function () {
-          toastr.success('{{ session('msg_success') }}');
-        });
-      
-      // 失敗時
-      @elseif (session('msg_error'))
-        $(function () {
-          toastr.error('{{ session('msg_error') }}');
-        });
-
-      @endif
-    </script>
-
+    @include('flash_message')
 
   </body>
 
