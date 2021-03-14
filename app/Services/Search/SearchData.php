60bd2965259978f0d57dfc5d6be27109ae8ee93b
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-03-14 20:11:01 +0900

    共通メソッドに切り離す

---
+++ app/Services/Search/SearchData.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Services\Search;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use App\Article;
+use App\Http\Controllers\Zoom\MeetingController;
+use App\Meeting;
+
+class SearchData
+{
+    /** キーワード検索
+     *  検索ボックスに入力されたキーワードとマッチするデータをDBから取得する
+     * @param string|null $keyword
+     * @param Model $model
+     * @return mixed
+     */
+    public static function searchKeyword(string $keyword = null, Model $model, Request $request)
+    {
+      $query = $model::query();
+
+      // もしキーワードがあったら
+      if ($keyword !== null) {
+        // 全角スペースを半角に
+        $keyword = mb_convert_kana($keyword, 's');
+
+        // 空白で区切る
+        $keywordSplit = preg_split('/[\s]+/', $keyword,-1,PREG_SPLIT_NO_EMPTY);
+
+        // ユーザー投稿をキーワードで検索
+        if ($model instanceof Article) {
+          // 単語をループで回す
+          foreach($keywordSplit as $value)
+          {
+              $query->where('body', 'like', '%'.$value.'%')
+                  ->with(['user', 'likes', 'tags']);
+          }
+        }
+
+        // ミーティングをキーワードで検索
+        if ($model instanceof Meeting) {
+          // 単語をループで回す
+          foreach($keywordSplit as $value)
+          {
+            $query->where('topic', 'like', '%'.$value.'%')
+                ->orWhere('agenda', 'like', '%'.$value.'%');
+          }
+        }
+      }
+
+      $paginate = $query
+          ->orderBy('created_at', 'desc')
+          ->paginate(10);
+
+          return $paginate;
+    }
+}
