6a3b782ff5e750e34f1d1780a04a43057dd4a94b
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-04-14 21:13:54 +0900

    不要なコードの削除

--- app/UploadImage.php
+++ app/UploadImage.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UploadImage extends Model
 {
-    use HasFactory;
-
     protected $table = "upload_image";
     protected $fillable = ["file_name", "file_path"];
 }
