71efc8980f233b9c6a6306dac46d75a1f65323c7
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-05-09 21:42:12 +0900

    画像アップロード画面の処理追加

--- resources/views/upload_form.blade.php
+++ resources/views/upload_form.blade.php
@@ -13,5 +13,22 @@
 <form method="POST" action="{{ route('upload_image') }}" enctype="multipart/form-data">
   @csrf
   <input type="file" name="image" accept="image/png, image/jpeg">
-  <input type="submit" value="Upload">
+
+  <p>
+    Preview:<br>
+    <img id="preview" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" style="max-width: 200px;">
+  </p>
+
+  <script>
+   function previewImage(obj)
+   {
+     var fileReader = new FileReader();
+     fileReader.onload = (function() {
+       document.getElementById('preview').src = fileReader.result;
+     });
+     fileReader.readAsDataURL(obj.files[0]);
+   }
+  </script>
+
+  <input type="submit" value="アップロード">
 </form>
